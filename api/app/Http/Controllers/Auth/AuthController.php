f80d7aff9b0efe0625a5a03a70f67d431401dd47
FREDVUNI <fredvuni809@gmail.com>
2023-07-12 16:34:25 +0300

    chore: Fix the signin -with try and catch

--- api/app/Http/Controllers/Auth/AuthController.php
+++ api/app/Http/Controllers/Auth/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -28,19 +27,17 @@ class AuthController extends Controller
         }
     }
     public function signin(){
-        $data = request()->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
-        if (Auth::attempt($data)) {
-            $user = request()->user();
+        try {
+            $data = request()->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            $user = User::userLogin($data);
             $token = $user->createToken('auth-token')->plainTextToken;
-
             return response()->json(['token' => $token], 200);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->errors()], 401);
         }
-
-        return response()->json(['message' => 'Invalid credentials'], 401);
     }
     public function logout(){
         request()->user()->currentAccessToken()->delete();
--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -43,6 +44,20 @@ class User extends Authenticatable
              'role' => $data['role'],
          ]);
      }
+     public static function userLogin($data)
+    {
+        $user = self::where('email', $data['email'])->first();
+
+        // dd($user);
+
+        if (!$user || !Hash::check($data['password'], $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Wrong email password combination.'],
+            ]);
+        }
+
+        return $user;
+    }
     protected $hidden = [
         'password',
         'remem
