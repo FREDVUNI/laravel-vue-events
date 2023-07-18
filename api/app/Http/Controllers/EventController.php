f5c124fa826dd43773e11d06c8a581eef5fe07bd
FREDVUNI <fredvuni809@gmail.com>
2023-07-18 15:48:30 +0300

    chore: test endpoint

--- api/app/Http/Controllers/EventController.php
+++ api/app/Http/Controllers/EventController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function events(){
+        return "yey! it works!";
+    }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\EventController;
 
 
 /*
