ea022ec3de4af617234878eaf0e54fe4799e018d
FREDVUNI <fredvuni809@gmail.com>
2023-07-18 15:52:49 +0300

    feat: create a get all events method

--- api/app/Http/Controllers/EventController.php
+++ api/app/Http/Controllers/EventController.php
@@ -1,12 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class EventController extends Controller
 {
     public function events(){
-        return "yey! it works!";
+        try{
+            $events = Event::all();
+            return response()->json(['events' => $events], 200);
+        }
+        catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
     }
 }
