25153be12107dc2029969f8c5caed2bd39ffe662
FREDVUNI <fredvuni809@gmail.com>
2023-07-18 16:07:53 +0300

    chore/feat: create a store method and separate logic

--- api/app/Http/Controllers/EventController.php
+++ api/app/Http/Controllers/EventController.php
@@ -3,15 +3,31 @@
 namespace App\Http\Controllers;
 use App\Models\Event;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
 class EventController extends Controller
 {
     public function events(){
         try{
-            $events = Event::all();
+            $events = Event::FetchEvents();
             return response()->json(['events' => $events], 200);
+        }
+        catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+    public function store(Request $request){
+        try{
+            $data = $request->validate([
+                "title" => "required|unique:events|min: 4|max:20",
+                "description" => "required| min: 4| max:200",
+                "slug" => "required",
+                "start_date" => "required|date_format:Y-m-d H:i:s|after:now",
+                "end_date" => "required|date_format:Y-m-d H:i:s|after:now",
+            ]);
+
+            $event = Event::createEvent($data);
+            return response()->json(['events' => $event], 201);
         }
         catch (\Exception $e) {
             return response()->json(['message' => 'Something went wrong.'], 500);
--- api/app/Models/Event.php
+++ api/app/Models/Event.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
     use HasFactory;
-
+    
     protected $guarded = [];
+
+    public static function FetchEvents(){
+        return self::all();
+    }
+    public static function createEvent(array $data){
+        return self::create([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'slug' => Str::slug($data['title']),
+            'start_date' => $data['start_date'],
+            'end_date' => $data['end_date'],
+        ]);
+    }
 }
