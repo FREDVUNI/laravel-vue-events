ceb669eac9c69731d23b9e808e3e5a1309c7dd62
FREDVUNI <fredvuni809@gmail.com>
2023-07-21 11:22:26 +0300

    feat: add attendee model methods

--- api/app/Models/Attendee.php
+++ api/app/Models/Attendee.php
@@ -10,4 +10,48 @@ class Attendee extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public static function fetchAttendees()
+    {
+        return self::all();
+    }
+    public static function createAttendee(array $data)
+    {
+        return self::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+        ]);
+    }
+    public static function getAttendee($slug)
+    {
+        $attendee = self::where('slug', $slug)->firstorFail();
+        return $attendee;
+    }
+    public static function showAttendee($slug)
+    {
+        $attendee = self::where('slug', $slug)->firstorFail();
+        return $attendee;
+    }
+    public static function editAttendee($slug)
+    {
+        $attendee = self::where('slug', $slug)->firstorFail();
+        return $attendee;
+    }
+    public static function updateAttendee($slug, array $data)
+    {
+        $attendee = self::where('slug', $slug)->firstorFail();
+        $attendee->update([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'phone' => $data['phone'],
+        ]);
+        return $attendee;
+    }
+    public static function deleteAttendee($slug)
+    {
+        $attendee = self::where('slug', $slug)->firstorFail();
+        $attendee->delete();
+        return $attendee;
+    }
 }
