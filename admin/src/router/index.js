0dd9f5ffd1223b9ec638438db1d9fbf2236c60ac
FREDVUNI <fredvuni809@gmail.com>
2023-07-27 10:45:29 +0300

    chore: add home component to routes

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 import RegisterView from "../views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: HomeView,
+    },
     {
       path: "/login",
       name: "login",
