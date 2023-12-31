f4b8eaa76656e1328bfc462479d515f3ebc788d2
Fred Vuni <fredvuni809@gmail.com>
2023-12-31 13:54:41 +0300

    chore: adjust the display

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,18 +1,20 @@
+<template>
+  <div id="app" class="flex h-screen">
+    <SideBar />
+    <div class="flex flex-1 flex-col">
+      <TopBar class="w-full" />
+      <div class="flex-1">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { RouterView } from "vue-router";
 import SideBar from "./components/SideBar.vue";
 import TopBar from "./components/TopBar.vue";
 </script>
-
-<template>
-  <div id="app">
-    <TopBar />
-    <div class="flex">
-      <SideBar />
-      <router-view />
-    </div>
-  </div>
-</template>
 
 <style scoped>
 /* Add any additional scoped styles if needed */
