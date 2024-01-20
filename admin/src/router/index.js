747d734e413b5a144388c2c4df382778fd59ed81
Fred Vuni <fredvuni809@gmail.com>
2024-01-20 14:21:22 +0300

    feat: add pinia for global state management

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -10,6 +10,7 @@ import AttendeeView from "../views/attendees/AttendeeView.vue";
 import AddAttendeeView from "../views/attendees/AddAttendeeView.vue";
 import EditAttendeeView from "../views/attendees/EditAttendeeView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
+import { useAuthStore } from "../stores/authStore";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,7 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: DashboardView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/login",
@@ -28,41 +30,49 @@ const router = createRouter({
       path: "/profile",
       name: "profile",
       component: ProfileView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/settings",
       name: "settings",
       component: RegisterView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/attendee-management",
       name: "attendees",
       component: AttendeeView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/attendee-management/create",
       name: "create-attendee",
       component: AddAttendeeView,
+      meta: { requiresAuth: true },
     },
     {
-      path: "/attendee-management/create",
+      path: "/attendee-management/edit/:id",
       name: "edit-attendee",
       component: EditAttendeeView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/event-management",
       name: "events",
       component: EventView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/event-management/create",
       name: "create-event",
       component: AddEventView,
+      meta: { requiresAuth: true },
     },
     {
-      path: "/event-management/create",
+      path: "/event-management/edit/:id",
       name: "edit-event",
       component: EditEventView,
+      meta: { requiresAuth: true },
     },
     {
       path: "/:catchAll(.*)",
@@ -70,6 +80,17 @@ const router = createRouter({
       component: NotFoundView,
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = useAuthStore().isAuthenticated;
+  console.log("isAuthenticated:", isAuthenticated);
+  console.log("to:", to);
+  if (to.matched.some((record) => record.meta.requiresAuth) && !isAuthenticated) {
+    next({ name: "signin" });
+  } else {
+    next();
+  }
 });
 
 export default router;
