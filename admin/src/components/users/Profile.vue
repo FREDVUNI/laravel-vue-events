10f6e8113f936456f766a0dddc9cb99d53f834f6
Fred Vuni <fredvuni809@gmail.com>
2024-01-20 12:34:51 +0300

    chore: refactor dashboard layout

--- admin/src/components/users/Profile.vue
+++ admin/src/components/users/Profile.vue
@@ -1,130 +1,99 @@
 <template>
-  <div class="container mx-auto py-8">
-    <div class="max-w-4xl mx-auto">
-      <div class="bg-white rounded-lg overflow-hidden">
-        <h1 class="text-2xl font-semibold mb-4 px-4">Profile</h1>
-        <div class="w-full md:w-1/2 px-4 items-center gap-x-4 mb-5 mt-5">
-          <div
-            class="relative w-20 h-20 rounded-full outline outline-offset-2 outline-1 outline-primary overflow-hidden"
-          ></div>
-          <button
-            type="button"
-            @click="handleDeleteImage"
-            class="rounded-lg px-4 py-2 text-red-500"
-          >
-            Delete
-          </button>
+  <div class="container max-w-md mx-auto py-8 h-full">
+    <div class="max-w-xl mx-auto bg-white rounded-lg overflow-hidden p-8">
+      <h1 class="text-2xl font-semibold mb-4">Profile</h1>
+      <div class="mb-8">
+        <div class="relative w-20 h-20 rounded-full bg-gray-200 mb-4">
+          <img
+            src="https://www.f6s.com/content-resource/profiles/3610847_th2.jpg"
+            alt="profile"
+            class="rounded-full bg-gray-200"
+          />
         </div>
-        <form @submit.prevent="submitHandler">
-          <div class="flex flex-wrap -mx-4">
-            <div class="w-full md:w-1/2 px-4">
-              <div class="p-4">
-                <label for="name" class="text-[#5a7184] font-semibold block"
-                  >Name</label
-                >
-                <input
-                  type="name"
-                  id="name"
-                  v-model="formData.name"
-                  @input="clearError('name')"
-                  class="w-full px-3 py-2"
-                  :class="{
-                    'placeholder:text-[#959ead]': true,
-                    'text-dark-hard': true,
-                    'mt-3': true,
-                    'rounded-lg': true,
-                    'px-5': true,
-                    'py-4': true,
-                    'font-semibold': true,
-                    block: true,
-                    'outline-none': true,
-                    border: true,
-                    'border-red-500': errors.name,
-                    'border-[#c3cad9]': !errors.name,
-                  }"
-                  placeholder="Enter name"
-                />
-                <p v-if="errors.name" class="text-red-500 text-xs mt-1">
-                  {{ errors.name }}
-                </p>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 px-4">
-              <div class="p-4">
-                <label for="email" class="text-[#5a7184] font-semibold block"
-                  >Email</label
-                >
-                <input
-                  type="email"
-                  id="email"
-                  v-model="formData.email"
-                  @input="clearError('email')"
-                  class="w-full px-3 py-2"
-                  :class="{
-                    'placeholder:text-[#959ead]': true,
-                    'text-dark-hard': true,
-                    'mt-3': true,
-                    'rounded-lg': true,
-                    'px-5': true,
-                    'py-4': true,
-                    'font-semibold': true,
-                    block: true,
-                    'outline-none': true,
-                    border: true,
-                    'border-red-500': errors.email,
-                    'border-[#c3cad9]': !errors.email,
-                  }"
-                  placeholder="Enter email"
-                />
-                <p v-if="errors.email" class="text-red-500 text-xs mt-1">
-                  {{ errors.email }}
-                </p>
-              </div>
-            </div>
-            <div class="w-full md:w-1/2 px-4">
-              <div class="p-4">
-                <label for="password" class="text-[#5a7184] font-semibold block"
-                  >Password</label
-                >
-                <input
-                  type="password"
-                  id="password"
-                  v-model="formData.password"
-                  @input="clearError('password')"
-                  class="w-full px-3 py-2"
-                  :class="{
-                    'placeholder:text-[#959ead]': true,
-                    'text-dark-hard': true,
-                    'mt-3': true,
-                    'rounded-lg': true,
-                    'px-5': true,
-                    'py-4': true,
-                    'font-semibold': true,
-                    block: true,
-                    'outline-none': true,
-                    border: true,
-                    'border-red-500': errors.password,
-                    'border-[#c3cad9]': !errors.password,
-                  }"
-                  placeholder="Enter password"
-                />
-                <p v-if="errors.password" class="text-red-500 text-xs mt-1">
-                  {{ errors.password }}
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full md:w-1/2 px-4">
-            <button
-              type="submit"
-              :disabled="!isValid || isLoading"
-              class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold text-lg py-4 px-8 rounded-lg my-6 disabled:opacity-70 disabled:cursor-not-allowed"
-            >
-              Update Profile
-            </button>
-          </div>
-        </form>
+        <button
+          type="button"
+          @click="handleDeleteImage"
+          class="text-red-500 font-semibold"
+        >
+          Delete
+        </button>
       </div>
+
+      <form @submit.prevent="submitHandler">
+        <!-- Name Field -->
+        <div class="mb-4">
+          <label for="name" class="block text-[#5a7184] font-semibold mb-2"
+            >Name</label
+          >
+          <input
+            type="name"
+            id="name"
+            v-model="formData.name"
+            @input="clearError('name')"
+            class="w-full px-4 py-2 rounded-lg border placeholder-[#959ead] text-dark-hard"
+            :class="{
+              'border-red-500': errors.name,
+              'border-[#c3cad9]': !errors.name,
+            }"
+            placeholder="Enter name"
+          />
+          <p v-if="errors.name" class="text-red-500 text-xs mt-1">
+            {{ errors.name }}
+          </p>
+        </div>
+
+        <!-- Email Field -->
+        <div class="mb-4">
+          <label for="email" class="block text-[#5a7184] font-semibold mb-2"
+            >Email</label
+          >
+          <input
+            type="email"
+            id="email"
+            v-model="formData.email"
+            @input="clearError('email')"
+            class="w-full px-4 py-2 rounded-lg border placeholder-[#959ead] text-dark-hard"
+            :class="{
+              'border-red-500': errors.email,
+              'border-[#c3cad9]': !errors.email,
+            }"
+            placeholder="Enter email"
+          />
+          <p v-if="errors.email" class="text-red-500 text-xs mt-1">
+            {{ errors.email }}
+          </p>
+        </div>
+
+        <!-- Password Field -->
+        <div class="mb-4">
+          <label for="password" class="block text-[#5a7184] font-semibold mb-2"
+            >Password</label
+          >
+          <input
+            type="password"
+            id="password"
+            v-model="formData.password"
+            @input="clearError('password')"
+            class="w-full px-4 py-2 rounded-lg border placeholder-[#959ead] text-dark-hard"
+            :class="{
+              'border-red-500': errors.password,
+              'border-[#c3cad9]': !errors.password,
+            }"
+            placeholder="Enter password"
+          />
+          <p v-if="errors.password" class="text-red-500 text-xs mt-1">
+            {{ errors.password }}
+          </p>
+        </div>
+
+        <button
+          type="submit"
+          :disabled="!isValid || isLoading"
+          class="w-full bg-indigo-500 hover:bg-indigo-600 text-white font-bold text-lg py-2 px-4 rounded-lg disabled:opacity-70 disabled:cursor-not-allowed"
+        >
+          Update Profile
+        </button>
+      </form>
     </div>
   </div>
 </template>
@@ -177,11 +146,14 @@ export default {
 
       // Make API call to update profile
       try {
-        const response = await axios.patch(`${import.meta.env.VITE_API_URL}/profile/update`, {
-          name: formData.name,
-          email: formData.email,
-          password: formData.password,
-        });
+        const response = await axios.patch(
+          `${import.meta.env.VITE_API_URL}/profile/update`,
+          {
+            name: formData.name,
+            email: formData.email,
+            password: formData.password,
+          }
+        );
 
         // Handle successful response
         console.log(response.data);
@@ -228,7 +200,7 @@ export default {
       isValid,
       submitHandler,
       handleDeleteImage,
-      fetchUserData
+      fetchUserData,
     };
   },
 };
