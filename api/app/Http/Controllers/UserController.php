ab0a08895fdd7b6b1e163d6545bc62fe3d098dfa
Fred Vuni <fredvuni809@gmail.com>
2024-01-21 12:59:28 +0300

    feat: apis for the users

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use \App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function Users()
+    {
+        try {
+            $users = User::fetchUsers();
+            return response()->json(['users' => $users], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+    public function show($id)
+    {
+        try {
+            $user = User::getUser($id);
+            return response()->json(['user' => $user], 200);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(['message' => 'User not found.'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+    public function edit($slug)
+    {
+        try {
+            $user = User::editUser($slug);
+            return response()->json(['user' => $user], 200);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(['message' => 'Event not found.'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        try {
+            $data = $request->validate([
+                'name' => 'required|min:4',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:6',
+                'role' => 'required'
+            ]);
+            $user = User::getUser($id);
+            $update = User::updateUser($id, $data);
+            return response()->json(['user' => $update], 200);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(['message' => 'user not found.'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+    public function delete($id)
+    {
+        try {
+            User::deleteUser($id);
+            return response()->json(["message" => "user has been deleted."]);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return response()->json(['message' => 'user not found.'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Something went wrong.'], 500);
+        }
+    }
+}
