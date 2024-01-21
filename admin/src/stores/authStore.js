487283541e8bdbe473a8472b4ea733c7aba9e289
Fred Vuni <fredvuni809@gmail.com>
2024-01-21 11:52:00 +0300

    chore: set up token in global state

--- admin/src/stores/authStore.js
+++ admin/src/stores/authStore.js
@@ -3,10 +3,19 @@ import { defineStore } from "pinia";
 export const useAuthStore = defineStore("auth", {
   state: () => ({
     isAuthenticated: false,
+    token: localStorage.getItem("token") || null,
   }),
   actions: {
     setAuthenticated(value) {
       this.isAuthenticated = value;
+    },
+    setToken(token) {
+      this.token = token;
+      localStorage.setItem("token", token);
+    },
+    clearToken() {
+      this.token = null;
+      localStorage.removeItem("token");
     },
   },
 });
