6564c998c06b9ce831d95d1e150756417f7113c3
Fred Vuni <fredvuni809@gmail.com>
2024-01-27 13:34:48 +0300

    chore: use vuetify table

--- admin/src/components/users/User.vue
+++ admin/src/components/users/User.vue
@@ -1,44 +1,72 @@
 <template>
   <section class="container mx-auto px-5 py-10">
-    <div class="flex justify-between items-center mb-4">
-      <h1 class="text-2xl font-semibold mb-4">Users</h1>
-      <router-link
-        to="/settings"
-        class="bg-blue-500 text-white px-3 py-1 rounded"
-        >Add</router-link
-      >
-    </div>
-    <div class="max-w-full overflow-x-auto">
-      <table
-        class="w-full sm:max-w-sm md:max-w-3xl lg:max-w-4xl xl:max-w-5xl text-left text-sm whitespace-nowrap"
-      >
-        <thead class="uppercase tracking-wider border-b">
-          <tr>
-            <th scope="col" class="px-6 py-4 font-semibold">Name</th>
-            <th scope="col" class="px-6 py-4 font-semibold">Email</th>
-            <th scope="col" class="px-6 py-4 font-semibold">Role</th>
-            <th scope="col" class="px-6 py-4 font-semibold">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            class="border-b dark:border-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-600"
-            v-for="(user, index) in usersData"
-            :key="index"
-          >
-            <td class="px-6 py-4">{{ user.name }}</td>
-            <td class="px-6 py-4">{{ user.email }}</td>
-            <td class="px-6 py-4">{{ user.role }}</td>
-            <td class="px-6 py-4">
-              <a class="text-green-500 hover:text-green-700" href="#"> Edit </a>
-              <a class="text-red-500 hover:text-red-700" href="#"> Delete </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="flex justify-between items-center mb-4">
+            <h1 class="text-2xl font-semibold mb-4">Users</h1>
+            <router-link
+              to="/settings"
+              class="bg-blue-500 text-white px-3 py-1 rounded"
+              >Add</router-link
+            >
+          </div>
+          <v-card flat>
+            <v-card-title class="d-flex align-center">
+              <div>
+                <v-text-field
+                  v-model="search"
+                  prepend-inner-icon="mdi-magnify"
+                  density="compact"
+                  label="Search"
+                  single-line
+                  flat
+                  hide-details
+                  variant="solo-filled"
+                  class="search-input"
+                ></v-text-field>
+              </div>
+            </v-card-title>
+            <v-divider></v-divider>
+
+            <v-data-table
+              v-model:search="search"
+              :items="usersData"
+              :items-per-page="5"
+            >
+              <template v-slot:headers="{ headers }">
+                <tr>
+                  <th v-for="header in headers" :key="header.value">
+                    <template>
+                      {{ header.text }}
+                    </template>
+                  </th>
+                </tr>
+              </template>
+              <template v-slot:item="{ item }">
+                <tr>
+                  <td v-if="!isSmallScreen">{{ item.id }}</td>
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.email }}</td>
+                  <td v-if="!isSmallScreen">{{ item.role }}</td>
+                  <td>
+                    <v-icon @click="editUser(item)" class="mx-2"
+                      >mdi-pencil</v-icon
+                    >
+                    <v-icon @click="deleteUser(item.id)" class="mx-2"
+                      >mdi-delete</v-icon
+                    >
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
+
 <script>
 import { ref, onMounted } from "vue";
 import axios from "axios";
@@ -47,25 +75,58 @@ import { url, setHeaders } from "../api";
 export default {
   setup() {
     const usersData = ref([]);
+    const search = ref("");
+    const isSmallScreen = ref(false);
+
+    const headers = [
+      { text: "#", value: "id" },
+      { text: "Name", value: "name" },
+      { text: "Email", value: "email" },
+      { text: "Role", value: "role" },
+      { text: "Actions", value: "actions" },
+    ];
 
     const fetchUsersData = async () => {
       try {
-        const response = await axios.get(`${url}users`, setHeaders());
+        const response = await axios.get(`${url}/users`, setHeaders());
         usersData.value = response.data.users;
-        // console.log(usersData.value);
       } catch (error) {
-        console.log(error);
+        console.error(error);
       }
+    };
+
+    const editUser = (user) => {
+      console.log("Edit user:", user);
+    };
+
+    const deleteUser = (userId) => {
+      console.log("Delete user with ID:", userId);
+    };
+
+    const checkScreenSize = () => {
+      isSmallScreen.value = window.innerWidth < 600;
     };
 
     onMounted(() => {
       fetchUsersData();
+      checkScreenSize();
+      window.addEventListener("resize", checkScreenSize);
     });
 
     return {
       usersData,
-      fetchUsersData,
+      search,
+      headers,
+      editUser,
+      deleteUser,
+      isSmallScreen,
     };
   },
 };
 </script>
+
+<style>
+.search-input {
+  width: 300px;
+}
+</style>
