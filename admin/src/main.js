26a4321aa7bca2a1da7712f73fa02468658dd164
Fred Vuni <fredvuni809@gmail.com>
2024-01-27 15:08:10 +0300

    feat: add date time picker

--- admin/src/main.js
+++ admin/src/main.js
@@ -6,7 +6,9 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import { useAuthStore } from "./stores/authStore";
 import router from "./router";
+import VueDatePicker from "@vuepic/vue-datepicker";
 import "vuetify/styles";
+import "@vuepic/vue-datepicker/dist/main.css";
 import "vue3-toastify/dist/index.css";
 
 import { createVuetify } from "vuetify";
@@ -25,6 +27,7 @@ const app = createApp(App);
 app.use(vuetify);
 app.use(pinia);
 app.use(router);
+app.component("VueDatePicker", VueDatePicker);
 
 app.component("useAuthStore", useAuthStore);
 app.mount("#app");
