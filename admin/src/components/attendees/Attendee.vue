efc0dfb8302682fff8a65c92a1a5bb67c547ad12
Fred Vuni <fredvuni809@gmail.com>
2024-01-27 15:08:38 +0300

    chore: fetch all attendees

--- admin/src/components/attendees/Attendee.vue
+++ admin/src/components/attendees/Attendee.vue
@@ -1,76 +1,133 @@
 <template>
   <section class="container mx-auto px-5 py-10">
-    <div class="flex justify-between items-center mb-4">
-      <h1 class="text-2xl font-semibold mb-4">Attendees</h1>
-      <router-link
-        to="/attendee-management/create"
-        class="bg-blue-500 text-white px-3 py-1 rounded"
-        >Add</router-link
-      >
-    </div>
-    <div class="max-w-full overflow-x-auto">
-      <table
-        class="w-full sm:max-w-sm md:max-w-3xl lg:max-w-4xl xl:max-w-5xl text-left text-sm whitespace-nowrap"
-      >
-        <thead class="uppercase tracking-wider border-b">
-          <tr>
-            <th scope="col" class="px-6 py-4">Name</th>
-            <th scope="col" class="px-6 py-4">Phone</th>
-            <th scope="col" class="px-6 py-4">Email</th>
-            <th scope="col" class="px-6 py-4">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            class="border-b dark:border-neutral-600 hover:bg-neutral-100 dark:hover:bg-neutral-600"
-            v-for="(event, index) in eventsData"
-            :key="index"
-          >
-            <td class="px-6 py-4">{{ event.name }}</td>
-            <td class="px-6 py-4">{{ event.phone }}</td>
-            <td class="px-6 py-4">{{ event.email }}</td>
-            <td class="px-6 py-4">
-              <a class="text-green-500 hover:text-green-700" href="#"> Edit </a>
-              <a class="text-red-500 hover:text-red-700" href="#"> Delete </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="flex justify-between items-center mb-4">
+            <h1 class="text-2xl font-semibold mb-4">Attendees</h1>
+            <router-link
+              to="/attendee-management/create"
+              class="bg-blue-500 text-white px-3 py-1 rounded"
+              >Add</router-link
+            >
+          </div>
+          <v-card flat>
+            <v-card-title class="d-flex align-center">
+              <div>
+                <v-text-field
+                  v-model="search"
+                  prepend-inner-icon="mdi-magnify"
+                  density="compact"
+                  label="Search"
+                  single-line
+                  flat
+                  hide-details
+                  variant="solo-filled"
+                  class="search-input"
+                ></v-text-field>
+              </div>
+            </v-card-title>
+            <v-divider></v-divider>
+
+            <v-data-table
+              v-model:search="search"
+              :items="attendeesData"
+              :items-per-page="5"
+            >
+              <template v-slot:headers="{ headers }">
+                <tr>
+                  <th v-if="!isSmallScreen">#</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th v-if="!isSmallScreen">Phone</th>
+                  <th></th>
+                </tr>
+              </template>
+
+              <template v-slot:item="{ item, index }">
+                <tr>
+                  <td v-if="!isSmallScreen">{{ index + 1 }}</td>
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.email }}</td>
+                  <td v-if="!isSmallScreen">{{ item.phone }}</td>
+                  <td>
+                    <v-icon @click="editAttendee(item)" class="mx-2"
+                      >mdi-pencil</v-icon
+                    >
+                    <v-icon @click="deleteAttendee(item.id)" class="mx-2"
+                      >mdi-delete</v-icon
+                    >
+                  </td>
+                </tr>
+              </template>
+            </v-data-table>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
+
 <script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { url, setHeaders } from "../api";
+
 export default {
-  data() {
+  setup() {
+    const attendeesData = ref([]);
+    const search = ref("");
+    const isSmallScreen = ref(false);
+
+    const headers = [
+      { text: "#", value: "id" },
+      { text: "attendee", value: "attendee" },
+      { text: "Date", value: "date" },
+      { text: "Location", value: "location" },
+      { text: "Actions", value: "actions" },
+    ];
+
+    const fetchattendeesData = async () => {
+      try {
+        const response = await axios.get(`${url}/attendees`, setHeaders());
+        attendeesData.value = response.data.attendees;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const editAttendee = (attendee) => {
+      console.log("Edit attendee:", attendee);
+    };
+
+    const deleteAttendee = (attendeeId) => {
+      console.log("Delete attendee with ID:", attendeeId);
+    };
+
+    const checkScreenSize = () => {
+      isSmallScreen.value = window.innerWidth < 600;
+    };
+
+    onMounted(() => {
+      fetchattendeesData();
+      checkScreenSize();
+      window.addattendeeListener("resize", checkScreenSize);
+    });
+
     return {
-      eventsData: [
-        {
-          name: "Event A",
-          phone: "2023-01-01",
-          email: "Venue X",
-        },
-        {
-          name: "Event B",
-          phone: "2023-02-02",
-          email: "Venue Y",
-        },
-        {
-          name: "Event C",
-          phone: "2023-03-03",
-          email: "Venue Z",
-        },
-        {
-          name: "Event D",
-          phone: "2023-04-04",
-          email: "Venue W",
-        },
-        {
-          name: "Event E",
-          phone: "2023-05-05",
-          email: "Venue P",
-        },
-      ],
+      attendeesData,
+      search,
+      headers,
+      editAttendee,
+      deleteAttendee,
+      isSmallScreen,
     };
   },
 };
 </script>
+
+<style>
+.search-input {
+  width: 300px;
+}
+</style>
