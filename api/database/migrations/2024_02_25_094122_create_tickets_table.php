997c1b6d10e77a7f692f1060c4b2ceb1035be56d
Fred Vuni <fredvuni809@gmail.com>
2024-02-25 13:04:52 +0300

    feat: create tickets schema

---
+++ api/database/migrations/2024_02_25_094122_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_type');
+            $table->decimal('price', 8, 2);
+            $table->string('slug')->unique();
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->foreignId('payment_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
