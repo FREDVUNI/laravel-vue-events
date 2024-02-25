42cd2aafd5c8f8f36546a13e8238792eaf03884e
Fred Vuni <fredvuni809@gmail.com>
2024-02-25 13:04:20 +0300

    feat: create the payment model

---
+++ api/app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'ticket_id',
+        'status',
+        'method',
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+}
