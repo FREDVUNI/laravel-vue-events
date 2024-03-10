166618a1b185334e4de4cffb7ab120620333744d
Fred Vuni <fredvuni809@gmail.com>
2024-03-10 13:49:29 +0300

    chore: refactor payment

--- api/app/Models/Payment.php
+++ api/app/Models/Payment.php
@@ -10,13 +10,22 @@ class Payment extends Model
     use HasFactory;
 
     protected $fillable = [
-        'ticket_id',
-        'payment_status',
         'payment_method',
+        'payment_status',
     ];
 
     public function ticket()
     {
         return $this->belongsTo(Ticket::class);
+    }
+
+    public static function createPayment(array $data)
+    {
+        $ticket = Ticket::findOrFail($data['ticket_id']);
+
+        return $ticket->payment()->create([
+            'payment_method' => $data['payment_method'],
+            'payment_status' => $data['payment_status'] ?? 'pending',
+        ]);
     }
 }
