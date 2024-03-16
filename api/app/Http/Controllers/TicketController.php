677d52809c743d4fd6a6e1a5c6e35acf45d86759
Fred Vuni <fredvuni809@gmail.com>
2024-03-16 13:49:48 +0300

    chore: fetch paid tickets

--- api/app/Http/Controllers/TicketController.php
+++ api/app/Http/Controllers/TicketController.php
@@ -34,7 +34,7 @@ class TicketController extends Controller
             $tickets = Ticket::FetchTickets();
             return response()->json(['tickets' => $tickets], 200);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Something went wrong.'], 500);
+            return response()->json(['message' => $e], 500);
         }
     }
 
