172effc9cf7962a2609e419ea612c7a24ddf7896
Fred Vuni <fredvuni809@gmail.com>
2026-04-06 15:20:58 +0300

    chore: set auth headers with the saved token

--- admin/src/components/api/index.js
+++ admin/src/components/api/index.js
@@ -4,13 +4,16 @@ import { useAuthStore } from "../../stores/authStore";
 export const url = `${import.meta.env.VITE_API_URL}`;
 
 export const setHeaders = () => {
-  const token = computed(() => useAuthStore().token);
+  const authStore = useAuthStore();
+  const token = authStore.token || localStorage.getItem("token");
 
-  const header = {
+  // console.log("Sending Token:", token);
+
+  return {
     headers: {
-      Authorization: `Bearer ${token.value || localStorage.getItem("token")}`,
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   };
-
-  return header;
 };
