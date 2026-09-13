476197246ed7a4245eaadfa77460b259c2c503e5
Fred Vuni <fredvuni809@gmail.com>
2026-09-13 13:04:21 +0300

    style: adjust formatting in router index.js for consistency

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -15,7 +15,7 @@ import { useAuthStore } from "../stores/authStore";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+  routes: [ 
     {
       path: "/login",
       name: "signin",
